0333321fcfbe270c542aff988ad06827d648d18b
aokad <aokada@ncc.go.jp>
2018-11-30 05:00:43 +0000

    update

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -30,73 +30,90 @@ class TestSet(unittest.TestCase):
     def tearDown(self):
         pass
     
-    # kibana
-    def test_kibana_01_version(self):
-        subprocess.check_call(['python3', 'elsu-kibana', '--version'])
-
-    def test_kibana_02_get_01(self):
-        options = ["all"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-
-    def test_kibana_02_get_02(self):
-        options = ["all"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-        
-    def test_kibana_02_get_03(self):
-        options = ["dashboard"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-
-    def test_kibana_02_get_04(self):
-        options = ["dashboard", "--title", "lassie01"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-
-    def test_kibana_02_get_05(self):
-        options = ["dashboard", "--id", "lassie01-hmp-2012-dashboard"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-
-    def test_kibana_02_get_06(self):
-        options = ["visualization"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-        
-    def test_kibana_02_get_07(self):
-        options = ["visualization", "--title", "bamsort-20181107-visualization-title"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-
-    def test_kibana_02_get_08(self):
-        options = ["visualization", "--id", "bamsort-20181107-visualization-title"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-
-    def test_kibana_02_get_09(self):
-        options = ["index-pattern"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-        
-    def test_kibana_02_get_10(self):
-        options = ["index-pattern", "--title", "c-cat-mutation-20181107-*-metrics-cpu"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-
-    def test_kibana_02_get_11(self):
-        options = ["index-pattern", "--id", "c-cat-mutation-20181107-*-metrics-cpu"]
-        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
-
     # ES
-    def test_es_01_version(self):
+    def test_es_version(self):
         subprocess.check_call(['python3', 'elsu-es', '--version'])
 
-    def test_es_02_get_01(self):
+    def test_es_post_01(self):
+        options = ["tests/testdata.json"]
+        subprocess.check_call(['python3', 'elsu-es', 'post'] + options)
+
+    def test_es_get_01(self):
         options = ["db"]
         subprocess.check_call(['python3', 'elsu-es', 'get'] + options)
 
-    def test_es_02_get_02(self):
+    def test_es_get_02(self):
         options = ["db", "--detail"]
         subprocess.check_call(['python3', 'elsu-es', 'get'] + options)
 
-    def test_es_02_get_03(self):
-        options = ["table", "--name", "loman-20181121"]
+    def test_es_get_03(self):
+        options = ["table", "--name", "annual_precipitation"]
         subprocess.check_call(['python3', 'elsu-es', 'get'] + options)
         
-    def test_es_02_get_04(self):
-        options = ["record", "--name", "loman-20181121"]
+    def test_es_get_04(self):
+        options = ["record", "--name", "annual_precipitation"]
         subprocess.check_call(['python3', 'elsu-es', 'get'] + options)
+
+    # kibana
+    def test_kibana_post_01(self):
+        options = ["index-pattern", "--index", "annual_precipitation"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'post'] + options)
+
+    """
+    def test_kibana_version(self):
+        subprocess.check_call(['python3', 'elsu-kibana', '--version'])
+    
+    def test_kibana_get_01(self):
+        options = ["all"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+
+    def test_kibana_get_02(self):
+        options = ["all"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+        
+    def test_kibana_get_03(self):
+        options = ["dashboard"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+
+    def test_kibana_get_04(self):
+        options = ["dashboard", "--title", "lassie01"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+
+    def test_kibana_get_05(self):
+        options = ["dashboard", "--id", "lassie01-hmp-2012-dashboard"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+
+    def test_kibana_get_06(self):
+        options = ["visualization"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+        
+    def test_kibana_get_07(self):
+        options = ["visualization", "--title", "bamsort-20181107-visualization-title"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+
+    def test_kibana_get_08(self):
+        options = ["visualization", "--id", "bamsort-20181107-visualization-title"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+    """
+    def test_kibana_get_09(self):
+        options = ["index-pattern"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+        
+    def test_kibana_get_10(self):
+        options = ["index-pattern", "--title", "annual_precipitation"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+
+    def test_kibana_get_11(self):
+        options = ["index-pattern", "--id", "annual_precipitation"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'get'] + options)
+    
+    def test_kibana_delete_01(self):
+        options = ["index-pattern", "--index", "annual_precipitation"]
+        subprocess.check_call(['python3', 'elsu-kibana', 'delete'] + options)
+
+    def test_es_delete_01(self):
+        options = ["annual_precipitation"]
+        subprocess.check_call(['python3', 'elsu-es', 'delete'] + options)
 
 def suite():
     suite = unittest.TestSuite()
